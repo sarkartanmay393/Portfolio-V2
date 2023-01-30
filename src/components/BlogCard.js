8ff9e21221ebe7faf53f9aa637c4c27daf00a23d
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-01-30 22:09:45 +0530

    Fix: Design in cards

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -6,6 +6,7 @@ function BlogSection() {
     // eslint-disable-next-line no-unused-vars
     const blogs = [
         {
+            colorCode: "green",
             views: "120",
             title: "Build Sudoku Solver Engine using Go",
             subtitle: `Solve your unsolved Sudoku patterns just in command. It takes a your pattern, solves it and writes in to a file.`,
@@ -14,6 +15,7 @@ function BlogSection() {
             image: `https://blog.tanmaysarkar.tech/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1667654155553%2FH-nukWZ2r.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75`,
         },
         {
+            colorCode: "lightpurple",
             views: "96",
             title: "Deploy your containerized Go Web Application",
             subtitle: `Today I am back with a new article on Go, but this is slightly in production side. Today we are going to deploy a simple containerized Go web app on Heroku.`,
@@ -22,6 +24,7 @@ function BlogSection() {
             image: `https://blog.tanmaysarkar.tech/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1666810316079%2F8b-KW6oLp.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75`,
         },
         {
+            colorCode: "yellow",
             views: "142",
             title: "Building a IMDB Web Scraper using Go",
             subtitle: `I thought about let's build a simple cli tool which will take birthday as input and crawl a tiny part of the website IMDB to look for celebrities who born on that date and stores various data into a file outputs/mm-dd.json inside output directory.`,
@@ -30,6 +33,7 @@ function BlogSection() {
             image: `https://blog.tanmaysarkar.tech/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1666263676327%2FouczJNN0b.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75`,
         },
     ];
+
 
     return (
         <section id='blogs'>
@@ -50,7 +54,7 @@ function BlogSection() {
                         <section className='blog-card'>
                             <section id='blog-card-main'>
                                 <h2>{value.title}</h2>
-                                <div className='row'>
+                                <div className='row' style={{color: `${value.colorCode}`}}>
                                     <p id='published-on'>{value.published_on}</p>
                                     <p id='read-time'>{value.time}</p>
                                     <p id='views'>{value.views} views</p>
