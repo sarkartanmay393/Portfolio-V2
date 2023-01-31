cd2e619968af063495b2cc6615eee0babcfd96a5
Tanmay Sarkar <sarkartanmay393@gmail.com>
2023-01-31 15:28:31 +0530

    Fix: Blog card minor

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -8,7 +8,7 @@ function BlogSection() {
         {
             link: `https://blog.tanmaysarkar.tech/build-sudoku-solver-engine-using-go`,
             colorCode: "lightgreen",
-            views: "120",
+            views: "281",
             title: "Build Sudoku Solver Engine using Go",
             subtitle: `Solve your unsolved Sudoku patterns just in command. It takes a your pattern, solves it and writes in to a file.`,
             published_on: `Nov 9, 2022`,
@@ -18,7 +18,7 @@ function BlogSection() {
         {
             link: `https://blog.tanmaysarkar.tech/deploy-your-containerized-go-web-application`,
             colorCode: "lightpurple",
-            views: "96",
+            views: "134",
             title: "Deploy your containerized Go Web Application",
             subtitle: `Today I am back with a new article on Go, but this is slightly in production side. Today we are going to deploy a simple containerized Go web app on Heroku.`,
             published_on: `Oct 27, 2022`,
@@ -28,7 +28,7 @@ function BlogSection() {
         {
             link: `https://blog.tanmaysarkar.tech/building-a-imdb-web-scraper-using-go`,
             colorCode: "yellow",
-            views: "142",
+            views: "438",
             title: "Building a IMDB Web Scraper using Go",
             subtitle: `I thought about let's build a simple cli tool which will take birthday as input and crawl a tiny part of the website IMDB to look for celebrities who born on that date and stores various data into a file outputs/mm-dd.json inside output directory.`,
             published_on: `Oct 21, 2022`,
@@ -42,19 +42,12 @@ function BlogSection() {
         <section id='blogs'>
             <header>
                 <h2><span>#</span>blogs</h2>
-                <a id="view-all-a" onClick={() => {
-                    window.document.getElementById('view-all-a').removeAttribute('href');
-                    let res = window.confirm("Visit blog.tanmaysarkar.tech");
-                    if (res) {
-                        window.open("https://blog.tanmaysarkar.tech/");
-                    }
-                    // window.document.getElementById('view-all-a').setAttribute('href', 'https://blog.tanmaysarkar.tech');
-                }} href='https://blog.tanmaysarkar.tech/'><button id='view-all-btn'>{viewAll}</button></a>
+                <a id="view-all-a" href='https://blog.tanmaysarkar.tech/' rel="noreferrer" target='_blank'><button id='view-all-btn'>{viewAll}</button></a>
             </header>
             <main>
-                {blogs.map((value, index) => {
+                {blogs.map((value, _) => {
                     return (
-                        <section className='blog-card' onClick={() => {window.open(value.link)}}>
+                        <section className='blog-card' onClick={() => { window.open(value.link) }}>
                             <section id='blog-card-main'>
                                 <a href={value.link}><h2>{value.title}</h2></a>
                                 <div className='blog-card-row'>
